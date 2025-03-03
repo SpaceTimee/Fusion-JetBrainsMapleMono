4ab2f332976e007305107070467c7b38554c1791
Space Time <Zeus6_6@163.com>
2025-03-03 16:32:33 +0800

    update

---
+++ fuse_fonts.py
@@ -0,0 +1,13 @@
+import sys
+from fontTools.ttLib import TTFont
+
+maple_font = TTFont(sys.argv[1])
+jetbrains_font = TTFont(sys.argv[2])
+
+for glyph in jetbrains_font.getGlyphNames():
+    if glyph in maple_font['glyf'].glyphs:
+        maple_font['glyf'].glyphs[glyph] = jetbrains_font['glyf'].glyphs[glyph]
+        if glyph in jetbrains_font['hmtx'].metrics:
+            maple_font['hmtx'].metrics[glyph] = jetbrains_font['hmtx'].metrics[glyph]
+
+maple_font.save('merged.ttf')
