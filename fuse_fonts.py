9caeceb20ebab5c751e70239475d53c27ab01104
Space Time <Zeus6_6@163.com>
2025-03-04 11:22:48 +0800

    Update fuse_fonts.py

--- fuse_fonts.py
+++ fuse_fonts.py
@@ -5,11 +5,11 @@ maple_font = TTFont(sys.argv[1])
 jetbrains_font = TTFont(sys.argv[2])
 
 for glyph in jetbrains_font.getGlyphNames():
-    if glyph in maple_font['glyf'].glyphs:
-        maple_font['glyf'].glyphs[glyph] = jetbrains_font['glyf'].glyphs[glyph]
-        if maple_font['glyf'].glyphs[glyph].isComposite():
-            maple_font['glyf'].glyphs[glyph].expand(jetbrains_font['glyf'])
-        if glyph in jetbrains_font['hmtx'].metrics:
-            maple_font['hmtx'].metrics[glyph] = jetbrains_font['hmtx'].metrics[glyph]
+    # if glyph in maple_font['glyf'].glyphs:
+    maple_font['glyf'].glyphs[glyph] = jetbrains_font['glyf'].glyphs[glyph]
+    if maple_font['glyf'].glyphs[glyph].isComposite():
+        maple_font['glyf'].glyphs[glyph].expand(jetbrains_font['glyf'])
+    if glyph in jetbrains_font['hmtx'].metrics:
+        maple_font['hmtx'].metrics[glyph] = jetbrains_font['hmtx'].metrics[glyph]
 
 maple_font.save('merged.ttf')
