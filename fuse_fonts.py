cc13c9f09413722b96fbde4419f7eada4de875d5
Space Time <Zeus6_6@163.com>
2025-03-05 10:42:27 +0800

    Update fuse_fonts.py

--- fuse_fonts.py
+++ fuse_fonts.py
@@ -7,6 +7,14 @@ jetbrains_font = TTFont(sys.argv[2])
 for glyph in jetbrains_font.getGlyphNames():
     # if glyph in maple_font['glyf'].glyphs:
     maple_font['glyf'].glyphs[glyph] = jetbrains_font['glyf'].glyphs[glyph]
+
+    if maple_font['glyf'].glyphs[glyph].isComposite():
+        try:
+            maple_font['glyf'].glyphs[glyph].expand(jetbrains_font['glyf'])
+        except RecursionError:
+            print(f"递归错误: {glyph}")
+            continue
+
     # if maple_font['glyf'].glyphs[glyph].isComposite():
     #     maple_font['glyf'].glyphs[glyph].expand(jetbrains_font['glyf'])
     if glyph in jetbrains_font['hmtx'].metrics:
